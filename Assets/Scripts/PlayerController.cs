1e8b0dbdecff9abdca34967cbfd0b5100d64dc9b
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-22 20:08:29 +1000

    Setup main scene
    - Add player object
    - Add player controller script
    - Position camera above player
    - Add camera script to follow player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent (typeof (Transform))]
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 5f;
+
+	Transform playerTransform;
+	Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GetComponent<Transform> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+		ResetPosition ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontalAxisOffset = Input.GetAxis ("Horizontal");
+		float verticalAxisOffset = Input.GetAxis ("Vertical");
+
+		Move (horizontalAxisOffset, verticalAxisOffset);
+	}
+
+	void Move(float horizontalAxisOffset, float verticalAxisOffset) {
+		Vector3 movement = new Vector3 (horizontalAxisOffset * movementSpeed * Time.deltaTime,  0f, verticalAxisOffset * movementSpeed * Time.deltaTime);
+
+		playerRigidbody.MovePosition (playerTransform.position + movement);
+	}
+
+	public void ResetPosition () {
+		playerTransform.position = new Vector3 (0f, 0.5f, 0f);
+	}
+
+	public void Die () {
+		SceneManager.LoadScene ("Main");
+	}
+}
