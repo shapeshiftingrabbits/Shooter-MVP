e6c349f36502c9242991c9617ba14f9d82cb35a0
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-24 22:36:54 +1000

    Player dies when it collides with an enemy
    - Dying resets the scene immediately

--- Assets/Scripts/EnemyBulletCollision.cs
+++ Assets/Scripts/EnemyBulletCollision.cs
@@ -6,8 +6,6 @@ public class EnemyBulletCollision : MonoBehaviour {
 	private string bulletTag = "Bullet";
 
 	void OnTriggerEnter (Collider other) {
-		Debug.Log ("Entered trigger");
-
 		if (other.gameObject.tag == bulletTag) {
 			Destroy (gameObject);
 		}
---
+++ Assets/Scripts/PlayerEnemyCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerEnemyCollision : MonoBehaviour {
+	private string enemyTag = "Enemy";
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == enemyTag) {
+			Die ();
+		}
+	}
+
+	public void Die () {
+		SceneManager.LoadScene ("Main");
+	}
+}
