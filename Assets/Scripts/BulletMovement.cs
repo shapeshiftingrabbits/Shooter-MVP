36c525c5a8b988eba4b68a76eb508bc8375768d6
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-24 18:19:39 +1000

    Add BulletMovement script

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+	private float movementSpeed = 5f;
+
+	void Update () {
+		gameObject.transform.position += nextPosition(Time.deltaTime);
+	}
+
+	Vector3 nextPosition(float deltaTime) {
+		return (gameObject.transform.up * deltaTime * movementSpeed);
+	}
+}
