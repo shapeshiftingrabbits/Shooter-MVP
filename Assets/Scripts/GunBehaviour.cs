5afcedc4e510ff235e7645483c3ebee919efa27f
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-24 18:03:19 +1000

    Set up gun behaviour to shoot bullets
    Bullets are only instanciated and to not move yet

---
+++ Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunBehaviour : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	private float fireRate = 1f;
+	private float lastShotInterval;
+
+	// Use this for initialization
+	void Start () {
+		lastShotInterval = fireRate;
+	}
+
+	void Update () {
+		lastShotInterval += Time.deltaTime;
+
+		if (Input.GetButton ("Fire") && lastShotInterval >= fireRate) {
+			Fire ();
+			lastShotInterval = 0f;
+		}
+	}
+
+	void Fire () {
+		Instantiate (bulletPrefab,  gameObject.transform.position, gameObject.transform.rotation);
+	}
+}
