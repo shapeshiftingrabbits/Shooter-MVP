fe9e69d8866ba272e11212506021202619483372
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-24 23:24:42 +1000

    Enemies move towards the player

---
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+	public GameObject player;
+	private float rotationSpeed = 5f;
+	private float movementSpeed = 3f;
+
+	void Update () {
+		Rotate (Time.deltaTime);
+		Move (Time.deltaTime);
+	}
+
+	void Rotate (float deltaTime) {
+		gameObject.transform.rotation = nextRotation (deltaTime);
+	}
+
+	void Move (float deltaTime) {
+		gameObject.transform.position += nextPosition(deltaTime);
+	}
+
+	Quaternion nextRotation(float deltaTime) {
+		return Quaternion.LookRotation(nextLookDirection (deltaTime));
+	}
+
+	Vector3 nextLookDirection(float deltaTime) {
+		return (
+			Vector3.RotateTowards (
+				gameObject.transform.forward,
+				player.transform.position - gameObject.transform.position,
+				deltaTime * rotationSpeed,
+				0f
+			)
+		);
+	}
+
+	Vector3 nextPosition(float deltaTime) {
+		return (gameObject.transform.forward * deltaTime * movementSpeed);
+	}
+}
