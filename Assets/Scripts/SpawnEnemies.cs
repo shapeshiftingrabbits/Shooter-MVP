ca8a6170d4349e8486876771968840037ba2463e
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-10-08 02:02:40 +1100

    Set spawnRate to increase with time and kills

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -3,24 +3,41 @@ using System.Collections;
 
 public class SpawnEnemies : MonoBehaviour {
 
-	public GameObject player;
+	public GameObject playerGameObject;
+	private PlayerScript playerScript;
 	public GameObject enemyPrefab;
+	float previousPlayTimeTick = 0f;
+	float currentPlayTime = 0f;
 	float spawnRate = 0.5f;
+	float minimumSpawnRate = 0.1f;
 	float spawnRateCounter;
 	int spawnMinRadius = 20;
 	int spawnMaxRadius = 50;
+	int nextPlayerKillsIncrease;
+	float spawnRateIncreaseSecondsInterval = 2f;
+	int spawnRateIncreaseKillsInterval = 10;
+	float spawnRateIncrease = 0.01f;
 
 	void Start () {
 		spawnRateCounter = spawnRate;
+		playerScript = playerGameObject.GetComponent<PlayerScript> ();
+		nextPlayerKillsIncrease = spawnRateIncreaseKillsInterval;
 	}
 
 	void Update () {
+		currentPlayTime += Time.deltaTime;
 		spawnRateCounter += Time.deltaTime;
 
 		if (spawnRateCounter >= spawnRate) {
 			SpawnEnemy ();
 			spawnRateCounter = 0f;
 		}
+
+		if (spawnRate > minimumSpawnRate) {
+			IncreaseSpawnRate ();
+		}
+
+		Debug.Log ("Spawn rate: " + spawnRate + ", " + (1f / spawnRate) + " enemies per second");
 	}
 
 	void SpawnEnemy () {
@@ -41,6 +58,18 @@ public class SpawnEnemies : MonoBehaviour {
 	}
 
 	Vector2 player2DPosition () {
-		return (new Vector2 (player.transform.position.x, player.transform.position.z));
+		return (new Vector2 (playerGameObject.transform.position.x, playerGameObject.transform.position.z));
+	}
+
+	void IncreaseSpawnRate() {
+		if (currentPlayTime - previousPlayTimeTick >= spawnRateIncreaseSecondsInterval) {
+			spawnRate = Mathf.Clamp (spawnRate - spawnRateIncrease, minimumSpawnRate, spawnRate);
+			previousPlayTimeTick = currentPlayTime;
+		}
+
+		if (playerScript.player.EnemiesKilled() == nextPlayerKillsIncrease) {
+			spawnRate = Mathf.Clamp (spawnRate - spawnRateIncrease, minimumSpawnRate, spawnRate);
+			nextPlayerKillsIncrease += spawnRateIncreaseKillsInterval;
+		}
 	}
 }
