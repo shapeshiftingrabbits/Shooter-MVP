e0ad80510a1929a1862b0bccd57c24dec4a77608
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-10-08 00:57:47 +1100

    Increment player kills when enemy dies

--- Assets/Scripts/EnemyBulletCollision.cs
+++ Assets/Scripts/EnemyBulletCollision.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class EnemyBulletCollision : MonoBehaviour {
 
+	private GameObject playerGameObject;
+	private PlayerScript playerScript;
 	private string bulletTag = "Bullet";
+
+	void Start () {
+		playerGameObject = GameObject.Find ("Player");
+		playerScript = playerGameObject.GetComponent<PlayerScript> ();
+	}
 
 	void OnTriggerEnter (Collider other) {
 		if (other.gameObject.tag == bulletTag) {
+			playerScript.player.incrementEnemyKills ();
+			Debug.Log (playerScript.player.EnemiesKilled());
 			Destroy (gameObject);
 			Destroy (other.gameObject);
 		}
