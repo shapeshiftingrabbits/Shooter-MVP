c9730a36cbf28230ba0e3c6a43c19069e11322f5
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-10-08 00:25:15 +1100

    Add Player script with Player class

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public class Player
+	{
+		private int enemiesKilled = 0;
+
+		public int EnemiesKilled () {
+			return enemiesKilled;
+		}
+
+		public void incrementEnemyKills () {
+			enemiesKilled++;
+		}
+	}
+
+	public Player player = new Player();
+}
