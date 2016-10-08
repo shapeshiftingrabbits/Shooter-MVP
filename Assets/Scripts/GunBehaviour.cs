b057970d31d6c9522e009e15b1526c03c571a91e
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-10-08 18:14:53 +1100

    Add new bullet model and adjust Gunbehaviour script

--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -6,8 +6,8 @@ public class GunBehaviour : MonoBehaviour {
 	public GameObject bulletPrefab;
 	private float fireRate = 0.2f;
 	private float lastShotInterval;
+	private Vector3 bulletStartPosition = new Vector3(0f, 0.01375f, 0f);
 
-	// Use this for initialization
 	void Start () {
 		lastShotInterval = fireRate;
 	}
@@ -22,6 +22,10 @@ public class GunBehaviour : MonoBehaviour {
 	}
 
 	void Fire () {
-		Instantiate (bulletPrefab,  gameObject.transform.position, gameObject.transform.rotation);
+		Instantiate (bulletPrefab, gameObject.transform.TransformPoint(bulletStartPosition), BulletRotation());
+	}
+
+	Quaternion BulletRotation () {
+		return Quaternion.FromToRotation(Vector3.up, gameObject.transform.forward);
 	}
 }
