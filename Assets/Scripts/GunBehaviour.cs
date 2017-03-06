1cbc5320d12d9921405c9113bf99707e0f4887f9
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-03-06 22:23:29 +1100

    Adjust force values and randomize force and torque output
    - Define minimum and maximum values for each aspect of the calculations and generate a random to give a sense of uniqueness to each casing movement
    - Take gun X scale into acount when calculating force

--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -9,7 +9,15 @@ public class GunBehaviour : MonoBehaviour {
 	private float lastShotInterval;
 	private Vector3 bulletStartPosition = new Vector3(0f, 0.01375f, 0f);
     private Vector3 bulletCasingStartPosition = new Vector3(0.00134f, 0.02091f, -0.00174f);
-    private float bulletCasingThrust = 10f;
+    private float bulletCasingThrust = 0.01f;
+	private float bulletCasingMinimumThrustDirectionUp = 2f;
+	private float bulletCasingMaximumThrustDirectionUp = 4f;
+	private float bulletCasingMinimumThrustDirectionRight = 4f;
+	private float bulletCasingMaximumThrustDirectionRight = 6f;
+	private float bulletCasingMinimumThrustDirectionForward = -2f;
+	private float bulletCasingMaximumThrustDirectionForward = -3f;
+	private float bulletCasingMinimumTorqueRight = -0.5f;
+	private float bulletCasingMaximumTorqueRight = -1.5f;
     private Animator gunAnimator;
 
 	void Start () {
@@ -31,15 +39,51 @@ public class GunBehaviour : MonoBehaviour {
         Instantiate(bulletPrefab, gameObject.transform.TransformPoint(bulletStartPosition), BulletRotation());
 
         GameObject bulletCasing = (GameObject)Instantiate(bulletCasingPrefab, gameObject.transform.TransformPoint(bulletCasingStartPosition), BulletRotation());
-        bulletCasing.GetComponent<Rigidbody>().AddForce(BulletCasingForceDirection() * bulletCasingThrust, ForceMode.Impulse);
-        bulletCasing.GetComponent<Rigidbody>().AddTorque(-gameObject.transform.right);
+        bulletCasing.GetComponent<Rigidbody>().AddForce(BulletCasingForce() * bulletCasingThrust, ForceMode.Impulse);
+        bulletCasing.GetComponent<Rigidbody>().AddTorque(BulletCasingTorque());
     }
 
 	Quaternion BulletRotation () {
 		return Quaternion.FromToRotation(Vector3.up, gameObject.transform.forward);
 	}
 
-    Vector3 BulletCasingForceDirection() {
-        return ((gameObject.transform.up / 2000) + (gameObject.transform.right / 6000));
+    Vector3 BulletCasingForce() {
+		return (
+			(
+				(
+					gameObject.transform.up *
+					Random.Range(
+						bulletCasingMinimumThrustDirectionUp,
+						bulletCasingMaximumThrustDirectionUp
+					)
+				)
+			) +
+			(
+				(
+					gameObject.transform.right *
+					gameObject.transform.localScale.x *
+					Random.Range(
+						bulletCasingMinimumThrustDirectionRight,
+						bulletCasingMaximumThrustDirectionRight
+					)
+				)
+			) +
+			(
+				(
+					gameObject.transform.forward *
+					Random.Range(
+						bulletCasingMinimumThrustDirectionForward,
+						bulletCasingMaximumThrustDirectionForward
+					)
+				)
+			)
+		);
     }
+
+	Vector3 BulletCasingTorque() {
+		return (
+			gameObject.transform.right *
+			Random.Range(bulletCasingMinimumTorqueRight, bulletCasingMaximumTorqueRight)
+		);
+	}
 }
