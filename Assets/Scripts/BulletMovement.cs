cb7f47ef612b9b1f75838ff820479ff806c2c151
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-03-11 00:08:18 +1100

    Fix missed collisions between bullets and enemies
    - Add rigidbody to bullets
    - Change bullet collider to a primitive capsule collider for better performance
    - Change bullet collider to non-trigger so bullet behaves like a solid object
    - Add force to bullet's rigidbody for movement
    - Set bullet's rigidbody collision detection to "continuous dynamic"
    - Set enemy's rigidbody collision detection to "continuous"

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -2,16 +2,6 @@
 using System.Collections;
 
 public class BulletMovement : MonoBehaviour {
-	private float movementSpeed = 25f;
-
-	void Update () {
-		gameObject.transform.position += nextPosition(Time.deltaTime);
-	}
-
-	Vector3 nextPosition(float deltaTime) {
-		return (gameObject.transform.up * deltaTime * movementSpeed);
-	}
-
 	void OnBecameInvisible () {
 		Destroy (gameObject);
 	}
