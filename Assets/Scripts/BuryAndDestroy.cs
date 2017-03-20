e2b86840c7a1afcdbd2ee4793205085c205b26a3
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-03-20 22:34:49 +1100

    Rename DestroyBulletCasing script to BuryAndDestroy

--- Assets/Scripts/BuryAndDestroy.cs
+++ Assets/Scripts/BuryAndDestroy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyBulletCasing : MonoBehaviour {
+public class BuryAndDestroy : MonoBehaviour {
 
 	Rigidbody bulletCasingRigidbody;
 	float elapsedTimeBeforeBurying = 0f;
