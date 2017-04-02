d401e7c1cc36869674b3c09aaabf3ddc2d9473e7
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-04-02 20:53:04 +1000

    Reduce enemy ragdoll light intensity over time

---
+++ Assets/Scripts/ReduceLightIntensityOverTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ReduceLightIntensityOverTime : MonoBehaviour {
+
+	public float speed = 1f;
+	private Light lightComponent;
+
+	void Start () {
+		lightComponent = gameObject.GetComponent<Light>();
+	}
+	
+	void Update () {
+		if (lightComponent.intensity > 0f) {
+			lightComponent.intensity -= speed * Time.deltaTime;
+		}
+	}
+}
