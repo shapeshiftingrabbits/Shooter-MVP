c7ec3882ad2c430bdeef3e7c355d46b4b2edebce
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-04-02 18:49:49 +1000

    Spawn enemy ragdoll when enemy dies
    Add bullet force to the ragdoll on each collision contact point

--- Assets/Scripts/EnemyBulletCollision.cs
+++ Assets/Scripts/EnemyBulletCollision.cs
@@ -2,20 +2,37 @@
 
 public class EnemyBulletCollision : MonoBehaviour {
 
+    public GameObject ragdollPrefab;
     private GameObject playerGameObject;
     private PlayerScript playerScript;
+    private GameObject enemyRagdoll;
+    private Rigidbody enemyRagdollRigidbody;
     private string bulletTag = "Bullet";
 
     void Start () {
         playerGameObject = GameObject.Find ("Player");
         playerScript = playerGameObject.GetComponent<PlayerScript> ();
+
+        enemyRagdoll = (GameObject) Instantiate(ragdollPrefab, gameObject.transform.position, gameObject.transform.rotation);
+        enemyRagdoll.SetActive(false);
+        enemyRagdollRigidbody = enemyRagdoll.GetComponent<Rigidbody>();
     }
 
     void OnCollisionEnter (Collision collision) {
         if (collision.gameObject.tag == bulletTag) {
             playerScript.player.incrementEnemyKills ();
-            Destroy (gameObject);
             Destroy (collision.gameObject);
+
+            enemyRagdoll.transform.position = gameObject.transform.position;
+            enemyRagdoll.transform.rotation = gameObject.transform.rotation;
+
+            Destroy (gameObject);
+           
+            enemyRagdoll.SetActive(true);
+
+            foreach (ContactPoint contactPoint in collision.contacts) {
+                enemyRagdollRigidbody.AddForceAtPosition(collision.relativeVelocity, contactPoint.point, ForceMode.Impulse);
+            }
         }
     }
 }
