77028252d124e63246723820c88c530d2efe75be
Laurie Trichet <laurie.trichet@gmail.com>
2017-06-25 22:42:36 +1000

    Add health bar to player
    - refactore the player taking damage into playerHealth and EnemyAttack.
    - Add slider as health bar.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	private string playerTag = "Player";
+	private GameObject player;
+	private PlayerHealth playerHealth;
+	public int attackPoints = 10;
+	
+	bool isPlayerInRange = false;
+	public float attackTimeThreshold = 1f;
+	float cumulTimeUpdate;
+
+	void Awake (){
+		player = GameObject.FindGameObjectWithTag (playerTag);
+        playerHealth = player.GetComponent <PlayerHealth> ();
+	}
+
+
+    void OnCollisionEnter (Collision collision) {
+        if (collision.gameObject == player) {
+            isPlayerInRange = true;
+        }
+    }
+
+	 void OnCollisionExit (Collision collision) {
+        if (collision.gameObject == player) {
+            isPlayerInRange = false;
+        }
+    }
+
+	void Update(){
+		UpdateTimer();
+		if (IsTimeToAttack() && isPlayerInRange){
+			Attack(player);
+			ResetTimer();
+		}
+	}
+
+	void Attack(GameObject player){
+		playerHealth.TakeDamage(attackPoints);
+	}
+
+	void UpdateTimer(){
+		cumulTimeUpdate += Time.deltaTime;
+	}
+
+	void ResetTimer(){
+		cumulTimeUpdate = 0;
+	}
+
+	bool IsTimeToAttack(){
+		return cumulTimeUpdate >= attackTimeThreshold;
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth;
+	public int currentHealth;
+	public Slider healthSlider;
+    private bool isDead;
+
+
+    void Awake(){
+		currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+		isDead = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage(int amount){
+		currentHealth -= amount;
+		UpdateHealthUI();
+		if (currentHealth <= 0 && !isDead){
+			Death();
+		}
+	}
+
+	void Death(){
+		isDead = true;
+		RestartLevel();
+	}
+
+	void UpdateHealthUI(){
+		healthSlider.value = currentHealth;
+	}
+
+	void RestartLevel(){
+        SceneManager.LoadScene ("Main");
+	}
+}
