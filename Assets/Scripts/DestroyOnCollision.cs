bbf21c9c3990cc7c76dc832c0274de32230210d8
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-07-11 21:19:19 +1000

    Update DestroyOnCollision to only destroy its own game object

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -1,16 +1,6 @@
 ﻿using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour {
-	
-	public bool destroySelf;
-	public bool destroyCollidingObject;
-
-	void OnCollisionEnter (Collision collision) {
-		if (destroyCollidingObject == true) {
-			Destroy(collision.collider);
-		}
-		if (destroySelf == true) {
-			Destroy(gameObject);
-		}
-	}
+    void OnCollisionEnter (Collision collision) {
+        Destroy(gameObject);
 }
