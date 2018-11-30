a65f630b2607e9526df86209874c30b6a035511e
Laurie Trichet <laurie.trichet@gmail.com>
2018-11-30 21:01:36 +1100

    add a game debug ui

---
+++ Assets/Scripts/UI/DebugInfoPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DebugInfoPanel : MonoBehaviour
+{
+  
+
+    void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+
+        style.alignment = TextAnchor.MiddleLeft;
+        style.fontSize = 20;
+        style.normal.textColor = Color.white;
+
+        Rect rect = new Rect(0, Screen.height - 50, 200, 50);
+
+        float msec = Time.smoothDeltaTime * 1000.0f;
+        float fps = 1.0f / Time.smoothDeltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+        GUI.Box(rect, text, style);
+    }
+
+}
+    
