e034dffcc281e8ae1ddf2a9f8db66339000f314b
Laurie Trichet <laurie.trichet@gmail.com>
2019-01-23 18:48:10 +1100

    ignore collider

--- Assets/Scripts/Utils/DestroyOnCollision.cs
+++ Assets/Scripts/Utils/DestroyOnCollision.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour {
+
+    private const string BULLET_TAG = "Bullet";
+
     void OnCollisionEnter (Collision collision) {
-        Destroy(gameObject);
+        if (collision.gameObject.tag == BULLET_TAG)
+        {
+            Debug.Log("ignore collision  with other bullet");
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        } else
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
